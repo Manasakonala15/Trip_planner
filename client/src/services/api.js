622814f370a6128f85763de8326dcdefc07f3a6f
Manasakonala15 <manasakonala15@gmail.com>
2026-05-17 20:41:35 +0530

    Change API base URL to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://trip-planner-e0vr.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
